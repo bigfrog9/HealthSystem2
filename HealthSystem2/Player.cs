5a9cbe3e634ea4fcda46b593abb4ec836022e651
John Wohler <johndoubleyou11@gmail.com>
2023-01-18 10:13:01 -0400

    Initial commit

---
+++ HealthSystem2/Player.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthSystem2
+{
+    internal class Player
+    {
+        private int health = 100;
+        private int lives = 3;
+
+        public void GetHealth(int hp)
+        {
+            health += hp;
+
+            if (health > 100) health = 100;
+
+        }
+
+        public void TakeDamage(int hp)
+        {
+            health -= hp;
+
+            if (health < 0) health = 0;
+
+            if (health == 0)
+            {
+                lives--;
+
+                health = 100;
+            }
+
+            if (lives == 0) health = 0;
+        }
+
+        public void ShowHud()
+        {
+
+            Console.WriteLine();
+            Console.WriteLine("Health: " + health);
+            Console.WriteLine("--------");
+            Console.WriteLine("Lives: " + lives);
+            Console.WriteLine();
+        }
+
+    }
+}
---
+++ HealthSystem2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthSystem2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Player player = new Player();
+
+            player.ShowHud();
+
+            player.TakeDamage(30);
+
+            player.ShowHud();
+
+            player.GetHealth(5);
+
+            player.ShowHud();
+
+            player.TakeDamage(120);
+
+            player.ShowHud();
+
+            Console.ReadKey(true);
+        }
+    }
+}
